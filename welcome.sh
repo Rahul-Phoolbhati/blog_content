6b572f9a354bf0d053e9eb5771018d105208bce3
Rahul-Phoolbhati <100228521+Rahul-Phoolbhati@users.noreply.github.com>
2023-04-10 00:33:05 +0530

    Add files via upload
    3 scripts added

---
+++ welcome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo  "Enter your name -   " 
+read name
+
+
+echo "Welcome $name."
+sleep 1
+echo "Today is $(date),
+Have a nice day!"
+sleep 1
+echo "You are currently in $(pwd) directory."
